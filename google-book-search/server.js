aa0c9b20fd1dd168a765601804a8152547dd8a35
Rubentba <rubenatorresbeltran@gmail.com>
2019-08-01 04:47:42 -0400

    Updated server.js with heroku information and apiroutes.

--- google-book-search/server.js
+++ google-book-search/server.js
@@ -1,24 +1,43 @@
-const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
+// Dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+
+// For Heroku
+const PORT = process.env.PORT || 3000;
 const app = express();
+
+// For Morgan
+app.use(logger('dev'));
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 }
 
 // Define API routes here
+// require('./app/routes/apiRoutes')(app);
+
+// DB URL
+const MONGODB_URI =
+  process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
+
+// Connect to the Mongo DB
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+// Making sure the server is live.
+app.listen(PORT, function() {
+  console.log(`App running on port ${PORT}!`);
 });
+
+module.exports = app;
